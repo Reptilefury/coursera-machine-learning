91865ea33bfa75dd5c09a35b441fd38c275ce79c
reptilefury <reptilefury@outlook.com>
2022-01-03 18:22:22 +0300

    Working on Data structures and algorithms

---
+++ bostonHousing.py
@@ -0,0 +1,19 @@
+import math
+
+import numpy as np
+
+b = 5.2
+
+output = np.log(b)
+
+print(np.log(b))
+print(math.e ** output)
+
+softmax = [0.7, 0.1, 0.2]
+target_output = [1, 0, 0]
+
+loss = -(math.log(softmax[0] * target_output[0] +
+                  softmax[1] * target_output[1] +
+                  softmax[2] * target_output[2]))
+
+print(loss)
